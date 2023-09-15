073df084a0b3a4b3f1159269c8aaedf7c18cdbb2
Tyler Hudson <tylerhudson96@gmail.com>
2023-09-15 12:34:06 -0400

    Gameboard methods for initializing. DOM caching, & cell click eventListeners

--- script.js
+++ script.js
@@ -1,5 +1,46 @@
-const gameBoard = function () {
-  "use strict";
+const gameboard = {
+  gameboard: ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X"],
+  // Initialize gameboard
+  init: function () {
+    this.cacheDom();
+    this.initEventListeners();
+  },
+  // DOM element targets
+  cacheDom: function () {
+    this.$gameboard = document.getElementById("gameboard");
+    this.$restartBtn = document.getElementById("restartButton");
+  },
+  initEventListeners: function () {
+    this.$gameboard.addEventListener("click", this.handleCellClicks);
+  },
+  // Update DOM with P1 marker
+  insertPlayerOne: function () {
+    // find target (event), update innerText to display marker
+  },
+  // Update DOM with P2 marker
+  insertPlayerTwo: function () {
+    // find target (event), update innerText to display marker
+  },
+  handleCellClicks: function (event) {
+    let selectedCell = event.target;
+    console.log(selectedCell);
+  },
+  //dev function to test DOM targeting - delete when no-longer needed
+  highlight: function (cell) {
+    if (selectedTd) {
+      // remove the existing highlight if any
+      selectedTd.classList.remove("highlight");
+    }
+    selectedTd = td;
+    selectedTd.classList.add("highlight"); // highlight the new td
+  },
+};
 
-  let gameBoardArr = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+gameboard.init();
+
+const players = {
+  players: [],
+  createPlayerOne: function (name) {
+    this.players.push(name);
+  },
 };
